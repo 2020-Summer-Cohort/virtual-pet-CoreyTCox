bc67daae3421a86f37d549302c501a6f62a23622
Jen E <jenevans3ten@gmail.com>
2020-05-21 21:42:35 -0400

    added comments with suggestions for future code

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -9,8 +9,47 @@ namespace VirtualPet
 {
     public class Pet
     {
-        private string name;
-        public string 
+        private string name;  // this is unnecessary if you add your properties like in line 16, you can remove it
+
+        // Properties Section ------------------ see Players Guide pg 136, Auto implemented Properties
+        public string Name { get; set; }
+
+        // add a Species property
+
+        // add a Hunger property
+
+        // add a Boredom property
+
+        // add a Health property
+
+
+        // Constructor Section --------------------- 
+
+        // add a Pet constructor that assigns Hunger to 50, Boredom to 60, and Health to 30
+
+
+        // Methods Section -------------------------- see Players guide pages 90-97
+
+        // finish adding the SetName method which takes in a string parameter, has no return value, and sets the
+        // Name property to the value passed in as a parameter
+
         public void SetName()
+        {
+            // this method was also missing the curly brace body
+        }
+
+        // add a method named SetSpecies that takes in a string parameter, has no return value, and sets the 
+        // Species property to the value passed in as a parameter
+
+        // add a method named Feed that has no parameters, has no return value, and decreases Hunger by 40
+
+        // add a method named SeeDoctor that has no parameters, has no return value, and increases Health by 30 
+
+        // add a method named Play that has no parameters, has no return value, and increases Hunger by 10,
+        // decreases Boredom by 20, and increases Health by 10
+
+        // add a method named Tick that has no parameters, has no return value, and increases Hunger by 5, increases
+        // Boredom by 5, and decreases Health by 5
+
     }
 }
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -7,6 +7,18 @@ namespace VirtualPet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! Welcome to Virtual Pets");
+
+            // Add a user menu here, that asks the user if they want to:
+            // 1. Feed your pet
+            // 2. Take your pet to the Doctor
+            // 3. Play with your pet
+            // 4. Exit
+
+            // Read the users response
+
+            // Add a switch case statement that handles each response- See Players Guide pgs 69-72
+
+
         }
     }
 }

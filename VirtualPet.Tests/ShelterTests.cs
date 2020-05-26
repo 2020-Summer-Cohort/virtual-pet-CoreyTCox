a34824fa07b00068e2d4c58698b513f0cc487cf3
Corey T Cox <coxcorey19@gmail.com>
2020-05-26 09:01:25 -0400

    added test names

--- VirtualPet.Tests/ShelterTests.cs
+++ VirtualPet.Tests/ShelterTests.cs
@@ -7,16 +7,50 @@ namespace VirtualPet.Tests
 {
     public class ShelterTests
     {
-        private Shelter testShelter;
+        Shelter testShelter;//could have used sut for "system under test"
         public ShelterTests()
         {
             testShelter = new Shelter();
         }
 
+        //admit pets into shelter and adopt
+        //see list of all pets in shelter
+        //see current status of all pets in shelter
+        //player can choose which pet(s) to interact with
         [Fact]
         public void Shelter_Constructor_Should_Instantiate_Shelter_Object()
         {
             Assert.NotNull(testShelter);
+        }
+
+        [Fact]
+        public void _Should_Instantiate_List()
+        {
+            Assert.NotNull(testShelter.ListOfPets);
+        }
+
+        [Fact]
+        public void Admit_Should_Add_Pet_To_Shelter()
+        {
+
+        }
+
+        [Fact]
+        public void Adopt_Should_Remove_Pet_From_Shelter()
+        {
+
+        }
+        
+        [Fact]
+        public void ListOfPets_Should_Print_List_Of_Pets_In_Shelter()
+        {
+
+        }
+
+        [Fact]
+        public void CurrentStatus_Should_Print_Status_Of_Each_Pet_In_Shelter()
+        {
+
         }
     }
 }
